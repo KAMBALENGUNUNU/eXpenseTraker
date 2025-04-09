762da71bae83fc5da7ba3cd39038c6a64261f307
priscamalunga <priscamalunga18@gmail.com>
2025-04-09 16:34:05 +0200

    transaction and report have been added,prisca malunga

---
+++ Pages/Transactions.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ExpenseTracker.Pages
+{
+    public class TransactionsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
